1943125a6fe116bfb8b569c3c9b760dd4a78f694
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-04-16 13:00:42 +0200

    refactor: change description

--- src/onStart.ts
+++ src/onStart.ts
@@ -4,7 +4,7 @@ export const onStart = new Composer();
 
 onStart.command("start", (ctx) => {
     ctx.reply(
-        "*Welcome\\!* \n\nWith me you can test your knowledge in Linux, DevOps, Networking, Programming, Cloud, Docker, Kubernetes and lots more!",
+        "*Welcome\\!* \n\nWith me you can test your knowledge in different topics",
         {
             parse_mode: "MarkdownV2",
         }
