65056fd6d88042a014a500edaa753c49c6791abf
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-04-16 13:00:12 +0200

    feat: add onQuiz

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,14 @@
 import { Bot } from "grammy";
 import { config } from "dotenv";
-import { onInfo } from "./onInfo";
-import { onStart } from "./onStart";
+import { onInfo } from "./onInfo.js";
+import { onStart } from "./onStart.js";
+import { onQuiz } from "./onQuiz.js";
 config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const bot: Bot = new Bot(BOT_TOKEN);
 
-bot.use(onStart).use(onInfo);
+bot.use(onStart).use(onInfo).use(onQuiz);
 
 bot.start();
